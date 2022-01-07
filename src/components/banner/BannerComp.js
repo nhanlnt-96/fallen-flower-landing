3dadb711e2baad79feea05a5acc5152732062af8
nhanlnt-96 <nhanlnt@hotmail.com>
2022-01-07 14:23:35 +0700

    [banner]: Complete create banner component

--- src/components/banner/BannerComp.js
+++ src/components/banner/BannerComp.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import {Container} from "react-bootstrap";
+import {Container, Row} from "react-bootstrap";
 import './BannerComp.scss';
 import HeaderComp from "../header/HeaderComp";
+import BannerImg from '../../assets/imgs/bgBanner.png';
 
 const BannerComp = () => {
   return (
     <Container fluid className="banner-comp comp-height">
       <HeaderComp/>
-      <Container className="banner-comp-container"></Container>
+      <Container className="banner-comp-container">
+        <div className="left-img"/>
+        <Row className="banner-comp-content d-flex justify-content-center align-items-center">
+          <img src={BannerImg} alt="fallen-flower-project"/>
+        </Row>
+        <div className="right-img"/>
+      </Container>
     </Container>
   );
 };
